2766109721dfb545e7aa015133c8289a4bce175a
01ena-shpak <shpaklenochka@gmail.com>
2026-03-28 02:22:35 +0200

    рандом 6 - 12 сек

--- PO_lab3.cpp
+++ PO_lab3.cpp
@@ -2,28 +2,34 @@
 #include <iostream>
 #include <thread>
 #include <chrono>
+#include <cstdlib>
+#include <ctime>
+
+Task generateRandomTask(int taskId)
+{
+    Task task;
+    task.id = taskId;
+    task.durationSeconds = 6 + std::rand() % 7; // 6 - 12
+    return task;
+}
 
 int main()
 {
+    std::srand(std::time(nullptr));
+
     ThreadPool pool;
     pool.initialize(4);
 
-    pool.submitTask({ 1, 10 });
-    pool.submitTask({ 2, 8 });
-    pool.submitTask({ 3, 12 });
-    pool.submitTask({ 4, 7 });
-    pool.submitTask({ 5, 9 });
-    pool.submitTask({ 6, 6 });
+    for (int i = 1; i <= 12; ++i)
+    {
+        Task task = generateRandomTask(i);
+        pool.submitTask(task);
 
-    std::this_thread::sleep_for(std::chrono::milliseconds(200));
+        std::this_thread::sleep_for(std::chrono::milliseconds(150));
+    }
 
-    pool.submitTask({ 7, 15 });
-    pool.submitTask({ 8, 11 });
-    pool.submitTask({ 9, 5 });
-    pool.submitTask({ 10, 4 });
-
-    std::cout << "[Main] Waiting 20 seconds for tasks to be processed...\n";
-    std::this_thread::sleep_for(std::chrono::seconds(20));
+    std::cout << "[Main] Waiting 30 seconds for tasks to be processed...\n";
+    std::this_thread::sleep_for(std::chrono::seconds(30));
 
     std::cout << "[Main] Now terminating pool...\n";
     pool.terminate();
